c3ab430ed68eaa98d6ab956b4fc04c837fce2ed2
Wei Dong <wdong@wdong.org>
2016-02-02 15:51:34 -0500

    added

---
+++ annotate/management/commands/import.py
@@ -0,0 +1,27 @@
+import sys
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
+from django.db import IntegrityError, transaction
+from django.contrib.auth.models import User
+from annotate.models import *
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--run', action='store_true', default=False, help='')
+        pass
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        #hours = options['hours'] + 24 * options['days']
+        #check_and_import(hours, not options['run'], options['check'])
+        run = options['run']
+        for line in sys.stdin:
+            path, meta = line.strip().split('\t')
+            if run:
+                Image.objects.create(path = path, meta = meta)
+            else:
+                print path, meta
+                pass
+            pass
+        pass
+
